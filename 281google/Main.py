e68f53d55e1e1279b937830f4d5ebc36bddec170
SAISRAVANICHALASANI2199 <91631318+SAISRAVANICHALASANI2199@users.noreply.github.com>
2021-10-12 17:59:08 -0700

    Adding files

---
+++ 281google/Main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import colorama
+
+
+# In[4]:
+
+
+from colorama import Fore
+
+
+# In[7]:
+
+
+print(Fore.BLUE + "HELLO GAO:HOW ARE YOU")
+
+
+# In[ ]:
+
+
+
+
